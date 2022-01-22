7b71f66b8bf1852a78ad8ebbb5e1e75926e38592
tienph91 <hoangtien.cp@gmail.com>
2022-01-22 11:03:14 +0700

    initial project

---
+++ src/test/java/com/example/springpetclinicpractice/SpringPetClinicPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpetclinicpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
