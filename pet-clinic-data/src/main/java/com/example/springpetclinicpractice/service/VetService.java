c300f1740076e0c2174dfd2e71a9b1450ed47c8a
tienph91 <hoangtien.cp@gmail.com>
2022-02-11 14:16:16 +0700

    #5 Implement Interface for services

---
+++ pet-clinic-data/src/main/java/com/example/springpetclinicpractice/service/VetService.java
@@ -0,0 +1,14 @@
+package com.example.springpetclinicpractice.service;
+
+import com.example.springpetclinicpractice.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
