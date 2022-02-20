be2a8b709eec3ede5a85e9e8c10de938c59a4e10
tienph91 <hoangtien.cp@gmail.com>
2022-02-20 11:46:09 +0700

    Close #8. Add index page

---
+++ pet-clinic-web/src/main/java/com/example/springpetclinicpractice/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.springpetclinicpractice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
